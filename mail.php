d906a6c67b23e79e68e9bd530bca622c7b928713
ALazyGuy <razorprogrammer@gmail.com>
2023-06-24 13:19:57 +0300

    feat: Implemented requests

--- mail.php
+++ mail.php
@@ -1,22 +1,31 @@
 <?php
 require 'PHPMailer/PHPMailerAutoload.php';
-$mail = new PHPMailer();
-$mail->isSMTP();                                    
-$mail->Host = 'smtp.yandex.ru';  
-$mail->SMTPAuth = true;                               
-$mail->Username = "testemailrobyline2@yandex.com";
-$mail->Password = "bwwhjtbynikdzrzf";                          
-$mail->SMTPSecure = 'tls';
-$mail->Port = 587;  
-$mail->isHTML(true);
-$mail->addAddress("testemailrobyline1@yandex.com", "Test1 Test1");
-$mail->setFrom("testemailrobyline2@yandex.com", "Test2 Test2");
-$mail->Subject = "Test is Test Email sent via Gmail SMTP Server using PHP Mailer";
-$mail->Body = "This is a Test Email sent via Gmail SMTP Server using PHP mailer class."; 
-if(!$mail->send()) {
-  echo "Error while sending Email.";
-  echo $mail->ErrorInfo;
-} else {
-  echo "Email sent successfully";
+
+$name = $_POST['name'];
+$company = $_POST['company'];
+$phone = $_POST['phone'];
+$email = $_POST['email'];
+$comment = $_POST['comment'];
+
+$message = 'Имя: ' . $name . '<br>Компания: ' . $company . '<br>Email: ' . $email . '<br>Телефон: ' . $phone . '<br>Комментарий: ' . $comment;
+
+if (isset($_POST['phone']) && isset($_POST['email'])) {
+  $mail = new PHPMailer();
+  $mail->isSMTP();
+  $mail->Host = 'smtp.yandex.ru';
+  $mail->SMTPAuth = true;
+  $mail->Username = "testemailrobyline2@yandex.com";
+  $mail->Password = "bwwhjtbynikdzrzf";
+  $mail->SMTPSecure = 'tls';
+  $mail->Port = 587;
+  $mail->isHTML(true);
+  $mail->addAddress("testemailrobyline1@yandex.com", "Test1 Test1");
+  $mail->setFrom("testemailrobyline2@yandex.com", "Администратор заявок");
+  $mail->Subject = "Заявка";
+  $mail->Body = $message;
+  $mail->CharSet = 'windows-1251';
+  $mail->send();
+  header('Location: /');
+  die();
 }
 ?>
