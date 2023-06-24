e50deb8e941982fe49fcd3b4597225b38a08521c
ALazyGuy <razorprogrammer@gmail.com>
2023-06-24 15:03:05 +0300

    feat: Implemented custom mask

--- index.js
+++ index.js
@@ -1,61 +1,74 @@
+let iti = null;
+
 class PhoneMask {
     constructor() {
         this.value = '';
+        this.mask = '';
+        this.previouseLength = 0;
+        this.maskLength = 0;
+    }
+
+    setMask(mask) {
+        this.mask = mask;
+        this.cleanMask = this.mask.split('').filter(c => /\d/.test(c)).join('');
+        this.maskLength = this.mask.length;
+    }
+
+    isValid() {
+        console.log(this.cleanMask + " " + this.value);
+        return this.cleanMask.length === this.value.length;
     }
 
     onInput(element) {
         let data = element.value;
-        if (data.length > 19) {
-            data = data.substring(0, 19);
-        }
-        if (data === '+375 ' || data === '+37' || data === '+3' || data === '+') {
-            this.value = '';
-        } else {
-            if (!data.startsWith('+375 (') && data.length > 0) {
-                this.value = data.split('').filter(c => /[0-9]/.test(c)).join('');
-            } else if (data.length > 0) {
-                const temp = data.substring(6);
-                this.value = temp.split('').filter(c => /[0-9]/.test(c)).join('');
-            }
+        if (data.length > this.maskLength) {
+            data = data.substring(0, this.maskLength);
         }
 
+        if(this.previouseLength > data.length) {
+            let done = false;
+            data = data.split('').reverse().filter(c => {                
+                if(done) {
+                    return true;
+                }
+
+                const isDigit = /\d/.test(c);
+                if(isDigit) {
+                    done = true;
+                    return true;
+                }
+
+                return false;
+            }).reverse().join('');
+        }
+
+        if (data.length > 0) {
+            this.value = data.split('').filter(c => /[0-9]/.test(c)).join('');
+        } else {
+            this.value = '';
+        }
+
+        this.previouseLength = data.length;
         element.value = this.getMaskedOutput();
     }
 
     getMaskedOutput() {
-        let result = '+375 (';
+        let result = '';
 
-        if(this.value.length > 0) {
-            if(this.value.length > 2) {
-                result += this.value.substring(0, 2) + ') ';
-            } else {
-                result += this.value.substring(0);
-            }
-        } else {
-            return result;
-        }
+        let counter = 0;
 
-        if(this.value.length > 2) {
-            if(this.value.length > 5) {
-                result += this.value.substring(2, 5) + '-';
-            } else {
-                result += this.value.substring(2);
-            }
-        }
+        for (let c of this.mask.split('')) {
+            if (/\d/.test(c)) {
+                if (this.value.length <= counter) {
+                    break;
+                }
 
-        if(this.value.length > 5) {
-            if(this.value.length > 7) {
-                result += this.value.substring(5, 7) + '-';
+                result += this.value[counter++];
+                if(counter === this.value.length) {
+                    break;
+                }
             } else {
-                result += this.value.substring(5);
-            }
-        }
-
-        if(this.value.length > 7) {
-            if(this.value.length > 9) {
-                result += this.value.substring(7, 9);
-            } else {
-                result += this.value.substring(7);
+                result += c;
             }
         }
 
@@ -75,10 +88,8 @@ window.onload = () => {
 
     const mask = new PhoneMask();
 
-    const validate = (element, pattern, onError) => {
-        const value = element.value;
-
-        if (!pattern.test(value)) {
+    const showValidationResults = (element, expression, onError) => {
+        if (!expression) {
             element.classList.add('invalid');
             onError.style.display = 'block';
             return false;
@@ -89,14 +100,21 @@ window.onload = () => {
         }
     };
 
+    const validate = (element, pattern, onError) => {
+        const value = element.value;
+        return showValidationResults(element, pattern.test(value), onError);
+    };
+
     const onFormFilled = (e) => {
         let success = true;
-        success = success * validate(phone, phoneRegExp, phoneInvalid);
+        success = success * showValidationResults(phone, mask.isValid(), phoneInvalid);
         success = success * validate(email, emailRegExp, emailInvalid);
 
         if (!success) {
             e.preventDefault();
         }
+
+        input.value = `+${iti.s.dialCode} ${input.value}`; 
     };
 
     const clean = (element, onError) => {
@@ -104,13 +122,27 @@ window.onload = () => {
         onError.style.display = 'none';
     };
 
+    mask.setMask('8 (912) 345-67-89');
+
     phone.onkeydown = () => clean(phone, phoneInvalid);
     email.onkeydown = () => clean(email, emailInvalid);
 
-    phone.onfocusout = () => validate(phone, phoneRegExp, phoneInvalid);
+    phone.onfocusout = () => showValidationResults(phone, mask.isValid(), phoneInvalid);
     email.onfocusout = () => validate(email, emailRegExp, emailInvalid);
 
     phone.addEventListener('input', e => mask.onInput(e.target));
 
     submit.onclick = onFormFilled;
+
+    const input = document.querySelector("#phone");
+    iti = window.intlTelInput(input, {
+        utilsScript: "https://cdn.jsdelivr.net/npm/intl-tel-input@18.1.1/build/js/utils.js",
+        autoPlaceholder: 'aggressive',
+        separateDialCode: true,
+        initialCountry: 'ru'
+    });
+
+    input.addEventListener("countrychange", function () {
+        mask.setMask(iti.a.placeholder);
+    });
 };
